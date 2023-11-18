4d5006c815b15fcb1ab09d12bf61727b7fd51ae3
Zach Van Camp <marshmilo100@gmail.com>
2023-11-18 14:32:31 -0600

    this works for now

--- wifi_audio/tcp_receiver/src/tcp_receiver.cpp
+++ wifi_audio/tcp_receiver/src/tcp_receiver.cpp
@@ -6,11 +6,10 @@
 #undef DEBUG
 
 ESPNowStream now;
-//I2SStream i2s;
 AnalogAudioStream i2s;
-AudioInfo info(32000,1,16); // may need to adjust
-EncodedAudioStream decoder(&i2s, new SBCDecoder(256)); // decode and write to I2S - ESP Now is limited to 256 bytes
-StreamCopy copier(decoder, now);
+AudioInfo info(44100, 1, 16); // may need to adjust
+EncodedAudioStream decoder(&i2s, new SBCDecoder(1024)); // decode and write to I2S - ESP Now is limited to 256 bytes
+StreamCopy copier(i2s, now);
 const char *peers[] = {"A8:48:FA:0B:93:02"};
 
 #ifdef DEBUG
@@ -26,13 +25,6 @@ void setup()
     auto config = i2s.defaultConfig(TX_MODE);
     config.copyFrom(info); 
     i2s.begin(config);
-    // auto i2s_config = i2s.defaultConfig(TX_MODE);
-    // i2s_config.copyFrom(info);
-    // i2s_config.signal_type = Analog;
-    // i2s_config.i2s_format = I2S_STD_FORMAT;
-    // i2s_config.use_apll = true;
-    // i2s_config.is_master = true;
-    // i2s.begin(i2s_config);
 
     Serial.println("Starting ESP-NOW...");
     auto cfg_now = now.defaultConfig();
@@ -41,7 +33,7 @@ void setup()
     now.addPeers(peers);
 
     Serial.println("Starting Decoder...");
-    decoder.begin();
+    //decoder.begin();
 
 #ifdef DEBUG
     csvStream.begin(info);
--- wifi_audio/tcp_sender/src/tcp_sender.cpp
+++ wifi_audio/tcp_sender/src/tcp_sender.cpp
@@ -1,74 +1,37 @@
-#include <Arduino.h>
+/**
+ * @file example-serial-send.ino
+ * @author Phil Schatzmann
+ * @brief Sending audio over ESPNow
+ * @version 0.1
+ * @date 2022-03-09
+ *
+ * @copyright Copyright (c) 2022
+ */
+
 #include "AudioTools.h"
 #include "AudioLibs/Communication.h"
-#include "AudioCodecs/CodecSBC.h"
 
-#undef DEBUG
-
-
-#define PIN_I2S_MCK   3
-#define c3_frequency  130.81
-#define VOLUME        100
-
-I2SStream i2s;
-ConverterFillLeftAndRight<int16_t> bothChannels(RightIsEmpty);
-AudioInfo info(32000,1,16); // may need to adjust
-
+AudioInfo info(44100, 1, 16);
+SineWaveGenerator<int16_t> sineWave( 32000);  // subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> sound( sineWave); // Stream generated from sine wave
 ESPNowStream now;
-SBCEncoder sbc;
-EncodedAudioStream encoder(&now, &sbc); // encode and write to ESP-now
-StreamCopy copier(encoder, i2s);  // copies sound into i2s
+StreamCopy copier(now, sound);  // copies sound into i2s
 const char *peers[] = {"A8:48:FA:0B:93:01"};
 
-#ifdef DEBUG
-CsvOutput<int32_t> csvStream(Serial);
-StreamCopy copier_serial(csvStream, i2s); // copy i2sStream to csvStream
-#endif
+void setup() {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
 
-const size_t max_buffer_len = 150;
-const int channels = 2;
-const size_t max_buffer_bytes = max_buffer_len * sizeof(int16_t) * channels;
-uint8_t buffer[max_buffer_bytes]={0};
+  auto cfg = now.defaultConfig();
+  cfg.mac_address = "A8:48:FA:0B:93:02";
+  now.begin(cfg);
+  now.addPeers(peers);
 
-// Arduino Setup
-void setup(void) 
-{
-    Serial.begin(115200);
-    AudioLogger::instance().begin(Serial, AudioLogger::Info);
-    
-    Serial.println("Starting I2S...");
-    auto i2s_config = i2s.defaultConfig(RX_MODE);
-    i2s_config.i2s_format = I2S_STD_FORMAT; // or try with I2S_LSB_FORMAT
-    i2s_config.pin_mck = 3;
-    i2s_config.use_apll = true;  // try with yes
-    i2s_config.copyFrom(info);
-    i2s_config.is_master = true;
-    i2s.begin(i2s_config);
-  
-    Serial.println("Starting ESP-NOW...");
-    auto now_config = now.defaultConfig();
-    now_config.mac_address = "A8:48:FA:0B:93:02";
-    now.begin(now_config);
-    now.addPeers(peers);
-
-    Serial.println("Starting Encoder...");
-    encoder.begin(info);
-
-#ifdef DEBUG
-    csvStream.begin(info);
-#endif
-
-    // Enable LED
-    pinMode(2, OUTPUT);
-    digitalWrite(2, HIGH);
+  // Setup sine wave
+  sineWave.begin(info, N_A4);
+  Serial.println("Sender started...");
 }
 
-// Arduino loop - repeated processing 
-void loop() 
-{
-    copier.copy();
-
-#ifdef DEBUG
-    copier_serial.copy();
-#endif /* DEBUG */
+void loop() { 
+  copier.copy();
 }
